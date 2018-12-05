2ac31f912d638015053819a85533c6fd0c35cd27
geoffrey jost <geoffrey@jostwebwerks.com>
2018-12-05 00:34:47 -0800

    Basic REST Framework setup for explorer app

--- conf/settings.py
+++ conf/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #
+    'rest_framework',
+    #
     'explorer',
 ]
 
@@ -129,3 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# REST interface
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- ddrexplorer/explorer/urls.py
+++ ddrexplorer/explorer/urls.py
@@ -1,4 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
+from rest_framework import routers
+router = routers.DefaultRouter()
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('api/v1/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/v1/', include(router.urls)),
 ]
