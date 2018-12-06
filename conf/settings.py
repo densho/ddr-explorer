cfe292057adb406f66fdd58389e53475ba142ef2
geoffrey jost <geoffrey@jostwebwerks.com>
2018-12-06 13:23:39 -0800

    set sqlite3 path in conf/settings

--- conf/settings.py
+++ conf/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'ddrexplorer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': config.get('app', 'sqlite_path'),
     }
 }
 
